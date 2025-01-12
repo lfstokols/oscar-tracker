c29c260809577955f31fa13f9eef29fd98ed0d1e
Logan Stokols <lfstokols@gmail.com>
2025-01-12 01:54:23 -0500

    Adding users should actually work now, untested: Adding missed file

---
+++ src/hooks/mutationOptions.tsx
@@ -0,0 +1,78 @@
+import {QueryClient} from '@tanstack/react-query';
+import {NotificationsDispatch} from '../globalProviders/NotificationContext';
+import {UserIdSchema, UserListSchema, UserSchema} from '../types/APIDataSchema';
+import {z} from 'zod';
+import {userOptions} from './dataOptions';
+
+// *
+// * Genera Stuff // *
+
+// *
+export function onSuccess<T>(
+  queryKey: any,
+  parser: (data: any) => T,
+  queryClient: QueryClient,
+) {
+  return async (response: Response) => {
+    return queryClient.setQueryData(queryKey, parser(await response.json()));
+  };
+}
+
+export function onError(message: string, notifications: NotificationsDispatch) {
+  return async (response: Response) => {
+    console.log(response);
+    notifications.show({
+      type: 'error',
+      message: message,
+    });
+  };
+}
+
+// *
+// * Mutation Functions // *
+// *
+
+export function updateWatchlistMutationFn(movieId: MovieId, year: number) {
+  return async (newState: WatchStatus) => {
+    const body = JSON.stringify({movieId, status: newState, year});
+    return await fetch('api/watchlist', {
+      method: 'PUT',
+      body,
+      headers: {'Content-Type': 'application/json'},
+    });
+  };
+}
+
+// this typecheck won't catch if you put a userId into `data`, but don't do that
+export function addUserMutationFn() {
+  return async ({
+    username,
+    data,
+  }: {
+    username: string;
+    data: Partial<z.input<typeof UserSchema>>;
+  }) => {
+    const body = JSON.stringify({username, ...data});
+    return await fetch('api/users', {
+      method: 'POST',
+      body,
+      headers: {'Content-Type': 'application/json'},
+    });
+  };
+}
+
+export function addUserOnSuccess(
+  queryClient: QueryClient,
+  setActiveUserId: (userId: UserId) => void,
+) {
+  return async (response: Response) => {
+    const data = await response.json();
+    const newId = UserIdSchema.parse(data.userId);
+    const newState = UserListSchema.parse(data.users);
+    setActiveUserId(newId);
+    queryClient.setQueryData(
+      userOptions().queryKey,
+      newState,
+    );
+  };
+}
