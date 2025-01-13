a2d6c80b63eb8f450138426d722d9dd8a8147354
Caleb Stokols <cjstokols@gmail.com>
2025-01-13 06:11:13 +0000

    Separate App into multiple components

---
+++ src/app/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import DefaultCatcher from '../components/LoadScreen';
+import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
+import SiteHeader from '../features/SiteHeader/SiteHeader';
+import AppProvider from './AppProvider';
+import AppContent from './AppContent';
+
+export default function App(): React.ReactElement {
+  return (
+    <DefaultCatcher>
+      <AppProvider>
+        <ReactQueryDevtools />
+        <div className="App" style={{
+          height: '100vh',
+          width: '100vw', 
+          overflow: 'hidden',
+          display: 'flex',
+          flexDirection: 'column'
+        }}>
+            <style>{`
+              ::-webkit-scrollbar {
+                display: none;
+              }
+              * {
+                scrollbar-width: none;
+                -ms-overflow-style: none;
+              }
+            `}</style>
+          <SiteHeader />
+          <AppContent />
+        </div>
+      </AppProvider>
+    </DefaultCatcher>
+  );
+}
+
+
+
