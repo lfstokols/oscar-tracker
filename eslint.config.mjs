3f22d4037cd94aab0b604545135fba631def0d5d
cjstokols <cjstokols@gmail.com>
2025-01-15 01:35:49 -0800

    trying to make eslint better

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,24 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+// import pluginJs from "@eslint/js";
+// import tseslint from "typescript-eslint";
+// import pluginReact from "eslint-plugin-react";
+// import pluginReactHooks from "eslint-plugin-react-hooks";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  {rules: {
-	semi: ["error", "always"],
-	"@typescript-eslint:no-unused-vars": "warn",
-  }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
+  // pluginJs.configs.recommended,
+  // ...tseslint.configs.recommended,
+  // pluginReact.configs.flat.recommended,
+  // pluginReactHooks.configs.flat.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      semi: ["error", "always"],
+      "@typescript-eslint:no-unused-vars": "warn",
+    }
+  },
 ];
