caa52c3cc4d0bf0912e7f2f02f20ee4f0ac377ad
lfstokols <lfstokols@gmail.com>
2025-01-17 20:32:51 -0500

    switched to loadEnv from dotenv in vite.config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,21 +1,23 @@
-import {defineConfig} from 'vite';
+import {defineConfig, loadEnv} from 'vite';
 import react from '@vitejs/plugin-react';
 //import vue from '@vitejs/plugin-vue';
-import {config} from 'dotenv';
+// import {config} from 'dotenv';
 
-config();
+// config();
 
-export default defineConfig({
-  plugins: [react()],
-  //plugins: [react(), vue()],
-  base: process.env.VITE_BUILD_BASE_URL,
-  server: {
-    port: process.env.VITE_PORT,
-    proxy: {
-      [process.env.VITE_BUILD_BASE_URL + '/api']: {
-        target: `http://127.0.0.1:${process.env.DEVSERVER_PORT}`,
-        changeOrigin: true,
+export default defineConfig(({command, mode}) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.VITE_BUILD_BASE_URL || '/',
+    plugins: [react()],
+    server: {
+      port: env.VITE_PORT,
+      proxy: {
+        [env.VITE_BUILD_BASE_URL + '/api']: {
+          target: `http://127.0.0.1:${env.DEVSERVER_PORT}`,
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
