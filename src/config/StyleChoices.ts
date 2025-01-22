a4bab3d52b684f9ef22a3002d1bd79967112bc78
lfstokols <lfstokols@gmail.com>
2025-01-21 21:15:08 -0500

    missed some files

---
+++ src/config/StyleChoices.ts
@@ -0,0 +1,12 @@
+export const BEST_PICTURE_COLOR = 'gold';
+export const HIGHLIGHT_ANIMATED_COLOR = 'lavender';
+
+export const TABLE_HEADER_COLOR = 'primary.main';
+export const TABLE_ROW_COLOR = 'secondary.dark';
+export const TABLE_ROW_MINOR_COLOR = 'secondary.main';
+export const BACKGROUND_COLOR = 'background';
+
+export const SEEN_COLOR = 'green';
+export const TODO_COLOR = 'goldenrod';
+export const NO_STATUS_COLOR = 'lightgrey';
+export const TODO_CONTRAST_COLOR = 'white';
