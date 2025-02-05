77f87cefa179d5b38844cbaf7b9593f2dd542c41
lfstokols <lfstokols@gmail.com>
2025-02-04 20:28:46 -0500

    Made nomination notes display on mobile

--- src/features/legacy_table/cells/NominationsCell.tsx
+++ src/features/legacy_table/cells/NominationsCell.tsx
@@ -1,17 +1,12 @@
 import {TableCell, Typography} from '@mui/material';
 import * as React from 'react';
 import {forwardRef, useEffect, useRef, useState} from 'react';
-import countries from '../../../assets/countries.json';
-import musicVideos from '../../../assets/musicVideos.json';
 import {ClickableTooltip} from '../../../components/ClickableTooltip';
-import {useIsMobile} from '../../../hooks/useIsMobile';
+import Entry from '../../../components/SingleNomEntry';
 import {CategoryList, MovieId, NomList} from '../../../types/APIDataSchema';
-import {Grouping} from '../../../types/Enums';
 
 import 'flag-icons/css/flag-icons.min.css';
 
-const countryCodes: {name: string; flag: string; code: string}[] = countries;
-const songUrls: {title: string; url: string}[] = musicVideos;
 const maxNomsToShow = 4;
 
 export default function NominationsCell({
@@ -73,92 +68,7 @@ export default function NominationsCell({
   );
 }
 
-function getFlag(country: string): React.ReactNode {
-  //* Convert country names to 2-letter ISO country codes
-  const data = countryCodes.find(c => c.name === country);
-  if (!data) return '';
-  return <span className={`fi fi-${data.code.toLowerCase()}`} />;
-}
 
-function getSong(song: string): React.ReactNode {
-  const url = songUrls.find(s => s.title === song)?.url;
-  if (!url) return song;
-  return (
-    <a
-      href={url}
-      style={{
-        marginLeft: '4px',
-        textDecoration: 'none',
-        color: 'inherit',
-        display: 'inline-flex',
-        alignItems: 'baseline',
-        gap: '4px',
-      }}>
-      <svg
-        height="1.25em"
-        style={{fill: '#FF0000', position: 'relative', top: '4px'}}
-        viewBox="0 0 24 24">
-        <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
-      </svg>
-      {song}
-    </a>
-  );
-}
-
-function getGroupingMarker(grouping: Grouping) {
-  switch (grouping) {
-    case Grouping.big_three:
-      return '⭐';
-    case Grouping.acting:
-      return '🎭';
-    case Grouping.art:
-      return '🎨';
-    case Grouping.audio:
-      return '🎧';
-    case Grouping.filmkraft:
-      return '🎞️';
-    case Grouping.best_in_class:
-      return '🥇';
-    case Grouping.short:
-      return '↔️';
-    default:
-      return '';
-  }
-}
-
-function Entry({
-  nom,
-  categories,
-}: {
-  nom: Nom;
-  categories: CategoryList;
-}): React.ReactElement {
-  const isMobile = useIsMobile();
-
-  const category = categories.find(cat => cat.id === nom.categoryId);
-  if (!category) {
-    return <Text>???</Text>;
-  }
-  const formattedNote =
-    category.id === 'cat_frgn' ? (
-      <em>
-        {getFlag(nom.note ?? '')} {nom.note ?? ''}
-      </em>
-    ) : category.id === 'cat_song' ? (
-      <>{getSong(nom.note ?? '')}</>
-    ) : (
-      <i>{nom.note}</i>
-    );
-
-  return (
-    <>
-      {getGroupingMarker(Grouping[category.grouping])}
-      {category.shortName + (!isMobile && category.hasNote ? ': ' : '')}
-      {!isMobile ? formattedNote : ''}
-      <br />
-    </>
-  );
-}
 
 function LineClampText({
   children,
